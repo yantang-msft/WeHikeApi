188dbd0dd5fde37f0bb4bed07c4fca44ba0f5706
yantang <yantang@microsoft.com>
2017-11-25 17:42:52 -0800

    Add missing utilities file

---
+++ RestApi/Utilities.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Web;
+using System.Web.Script.Serialization;
+
+namespace RestApi
+{
+    public class Utilities
+    {
+        public static HttpResponseMessage CreateJsonReponse(HttpRequestMessage request, object result)
+        {
+            HttpResponseMessage response = request.CreateResponse(HttpStatusCode.OK);
+            response.Content = new StringContent(new JavaScriptSerializer().Serialize(result), Encoding.UTF8, "application/json");
+
+            return response;
+        }
+    }
+}
