b4709100c08619f867dd1a35c52644ff39a0220d
Yanming Tang <yantang@microsoft.com>
2017-11-25 20:26:09 -0800

    Fix database connection string

--- RestApi/Constants.cs
+++ RestApi/Constants.cs
@@ -7,7 +7,7 @@ namespace RestApi
 {
     public class Constants
     {
-        public static readonly string ConnectionStr = "Server=tcp:wehike.database.windows.net,1433;Initial Catalog=WeHike;Persist Security Info=False;User ID=wehike;Password=#Bugsfor";
+        public static readonly string ConnectionStr = "Server=tcp:wehike.database.windows.net,1433;Initial Catalog=WeHike;Persist Security Info=False;User ID=wehike;Password=#Bugsfor$;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
         public static readonly string AuthorizationHeader = "AuthorizationHeader";
     }
 }
